727ea1a20f626cb1e3b3b40c1ded2164ed913160
satyendra123 <Singhsatyendra885@gmail.com>
2025-05-20 16:34:17 +0530

    Update skymarkcontroller.ino

--- skymarkcontroller.ino
+++ skymarkcontroller.ino
@@ -2,10 +2,29 @@
 #include <SPI.h>
 #include <Ethernet.h>
 
-byte mac[] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED};
+// MAC and IP for entry_gate1
+byte mac[] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0x01};
 IPAddress ip(192, 168, 1, 157);
+
+// For entry_gate2
+// byte mac[] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0x02};
+// IPAddress ip(192, 168, 1, 158);
+
+// For exit_gate1
+// byte mac[] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0x03};
+// IPAddress ip(192, 168, 1, 159);
+
+// For exit_gate2
+// byte mac[] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0x04};
+// IPAddress ip(192, 168, 1, 160);
+
 IPAddress serverIp(192, 168, 1, 50); // IP where Flask runs
 EthernetClient client;
+
+//const char* gateID = "entry_gate1";
+// const char* gateID = "entry_gate2";
+// const char* gateID = "exit_gate1";
+// const char* gateID = "exit_gate2";
 
 const int relayPin = 9;
 
