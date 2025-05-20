8ef7ec1af1aba137c7d41afadd70d398441ef65a
satyendra123 <Singhsatyendra885@gmail.com>
2025-05-20 16:32:21 +0530

    Update skymarkcontroller.ino

--- skymarkcontroller.ino
+++ skymarkcontroller.ino
@@ -1,4 +1,49 @@
+// tis is the deployed code and it is workin fine. in this code whether the server is starting first or client is starting first it will work
 #include <SPI.h>
+#include <Ethernet.h>
+
+byte mac[] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED};
+IPAddress ip(192, 168, 1, 157);
+IPAddress serverIp(192, 168, 1, 50); // IP where Flask runs
+EthernetClient client;
+
+const int relayPin = 9;
+
+void setup() {
+  Ethernet.begin(mac, ip);
+  Serial.begin(9600);
+  pinMode(relayPin, OUTPUT);
+  digitalWrite(relayPin, HIGH);  // Relay off (active LOW)
+}
+
+void loop() {
+  if (client.connect(serverIp, 5000)) { // Flask running on port 8000
+    client.println("GET /check_boomsig?gate_id=entry_gate1 HTTP/1.1");
+    client.println("Host: 192.168.1.100");
+    client.println("Connection: close");
+    client.println();
+
+    while (client.connected()) {
+      if (client.available()) {
+        String line = client.readStringUntil('\n');
+        Serial.println(line);
+
+        if (line.indexOf("|OPENEN%") >= 0) {
+          Serial.println("Opening gate...");
+          digitalWrite(relayPin, LOW);
+          delay(500);
+          digitalWrite(relayPin, HIGH);
+        }
+      }
+    }
+    client.stop();
+  } else {
+    Serial.println("Connection to server failed");
+  }
+  delay(5000);
+}
+
+//Ex-2 this is the deployed code and it is working but the problem is the server should be start first then the arduino client will be connected. but when power fails then it will not send the data to the server
 #include <Ethernet.h>
 
 #define relayPin 9
